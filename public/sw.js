537b6e8c14bc50e36fb070e56cf00cad4019844e
1614130601zjs-del <1614130601zjs@gmail.com>
2026-09-04 08:41:44 +0800

    chore: bump service worker cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'charpet-shell-v1';
+const CACHE = 'charpet-shell-v2';
 const APP_SHELL = ['/', '/index.html', '/manifest.webmanifest'];
 
 self.addEventListener('install', event => {
@@ -6,7 +6,9 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', event => {
